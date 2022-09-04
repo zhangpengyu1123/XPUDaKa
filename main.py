d9ebf162c3f152d40d950ff1c27e52fba38db4a5
wyx <1105017602@qq.com>
2022-09-04 20:28:06 +0800

    取消异常处理，直接报错，这样才会通过邮箱提醒

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 from urllib.parse import urlencode
 import os
 
-
 info = os.environ.get('INFO', '').split('\n')
 answers = os.environ.get('ANS', '').split('\n')
 agent = os.environ.get('AGENT', '')
@@ -48,9 +47,6 @@ def login(username, password):
         # 登录成功获取JWSESSION
         jwsession = response.headers['JWSESSION']
         return jwsession
-    else:
-        print("登陆失败，请检查账号信息和密码是否正确")
-        return False
 
 
 def getLoction(latitude, longitude):
@@ -88,49 +84,46 @@ class Do:
     def run(self):
         lo = login(username, password)
 
-        if not lo:
-            print("登录失败!有可能是账号或密码错误")
+        url = "https://student.wozaixiaoyuan.com/health/save.json"  # 健康打卡 提交地址
+        locationList = getLoction(latitude, longitude)
+
+        # 健康打卡数据
+        data = {
+            "answers": answers,
+            "latitude": latitude,  # 维度
+            "longitude": longitude,  # 经度
+            "country": "中国",
+            "city": locationList[0],
+            "district": locationList[1],
+            "province": locationList[2],
+            "township": locationList[3],
+            "street": locationList[4],
+            "areacode": locationList[5],
+            "towncode": locationList[6],
+            "citycode": locationList[7]
+        }
+
+        time_t = int(round(time.time() * 1000))
+
+        content = f"{locationList[2]}_{time_t}_{locationList[0]}"
+        signature = hashlib.sha256(content.encode('utf-8')).hexdigest()
+
+        data["timestampHeader"] = time_t
+        data["signatureHeader"] = signature
+
+        self.headers["JWSESSION"] = lo
+        print("JWsession--> " + self.headers["JWSESSION"])
+        print("当前时间--> ", datetime.datetime.now())
+        data = urlencode(data)
+        res = requests.post(url=url, headers=self.headers, data=data)  # 健康打卡提交
+        res_text = res.json()
+        print("res-->", res_text)
+        if res_text["code"] == 0:
+            print("打卡成功")
+        elif res_text["code"] == 1:
+            print("message", res_text["message"])
         else:
-            url = "https://student.wozaixiaoyuan.com/health/save.json"  # 健康打卡 提交地址
-            locationList = getLoction(latitude, longitude)
-
-            # 健康打卡数据
-            data = {
-                "answers": answers,
-                "latitude": latitude,  # 维度
-                "longitude": longitude,  # 经度
-                "country": "中国",
-                "city": locationList[0],
-                "district": locationList[1],
-                "province": locationList[2],
-                "township": locationList[3],
-                "street": locationList[4],
-                "areacode": locationList[5],
-                "towncode": locationList[6],
-                "citycode": locationList[7]
-            }
-
-            time_t = int(round(time.time() * 1000))
-
-            content = f"{locationList[2]}_{time_t}_{locationList[0]}"
-            signature = hashlib.sha256(content.encode('utf-8')).hexdigest()
-
-            data["timestampHeader"] = time_t
-            data["signatureHeader"] = signature
-
-            self.headers["JWSESSION"] = lo
-            print("JWsession--> " + self.headers["JWSESSION"])
-            print("当前时间--> ", datetime.datetime.now())
-            data = urlencode(data)
-            res = requests.post(url=url, headers=self.headers, data=data)  # 健康打卡提交
-            res_text = res.json()
-            print("res-->", res_text)
-            if res_text["code"] == 0:
-                print("打卡成功")
-            elif res_text["code"] == 1:
-                print("message", res_text["message"])
-            else:
-                print("打卡失败")
+            print("打卡失败")
 
 
 if __name__ == "__main__":
